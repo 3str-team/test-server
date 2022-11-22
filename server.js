f844b44493f13264edb3e02bc262d2bc6df80d27
3str <ssrghome@gmail.com>
2022-11-22 13:38:08 +0300

    test2

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
+app.get("/hello", (req, res) => {
+  console.log("Request");
+  res.json("Hello world");
 });
 
 app.listen(port, () => {
